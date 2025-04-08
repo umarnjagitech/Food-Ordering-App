4973106302da80f3392daf5568505a9a18cf7768
umarnjagitech <omarnjagi@gmail.com>
2025-04-08 15:56:20 +0300

    Added a few assets and default files from the Tutorial Projects Repo.

--- app/src/main/java/com/umarndungotech/food_ordering_app/ui/theme/Type.kt
+++ app/src/main/java/com/umarndungotech/food_ordering_app/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.umarndungotech.food_ordering_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.umarndungotech.food_ordering_app.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,4 @@ val Typography = Typography(
     )
     */
 )
+val pacificoFont = FontFamily(Font(R.font.pacifico))
